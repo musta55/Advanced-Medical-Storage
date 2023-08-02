4b7b220b639658f259374b63385d7cd8d504ce90
musta55 <bsse1114@iit.du.ac.bd>
2023-08-03 01:17:33 +0600

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,23 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+
+require("@nomiclabs/hardhat-ethers");
+
+module.exports = {
+  solidity: {
+    version: '0.8.9',
+    defaultNetwork: 'sepolia',
+    networks: {
+      hardhat: {},
+      sepolia: {
+        url: process.env.REACT_APP_ALCHEMY_API_URL,
+        accounts: [ process.env.REACT_APP_PRIVATE_KEY ]
+      }
+    },
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+};
